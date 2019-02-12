4a083159e20b444ac44340884d9563244ba91f7e
Jonas DOREL <jonas.dorel@laposte.net>
2019-02-12 22:44:17 +0100

    Replace qdbus with dbus-send

--- check-metered-connection.sh
+++ check-metered-connection.sh
@@ -1,6 +1,11 @@
 #! /usr/bin/bash
 
-metered_status=$(qdbus --system org.freedesktop.NetworkManager /org/freedesktop/NetworkManager org.freedesktop.NetworkManager.Metered)
+metered_status=$(dbus-send --system --print-reply=literal \
+       	--system --dest=org.freedesktop.NetworkManager \
+	      /org/freedesktop/NetworkManager \
+	      org.freedesktop.DBus.Properties.Get \
+	      string:org.freedesktop.NetworkManager string:Metered \
+	      | grep -o ".$")
 
 if [[ $metered_status =~ (1|3) ]]; then
   echo Current connection is metered
